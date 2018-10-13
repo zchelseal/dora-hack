9afd5fa16c3843d07136e0d14e612fc4c3442ad4
zzq229 <zzqwaterloo@gmail.com>
2018-10-13 15:08:35 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var createError = require('http-errors')
+var express = require('express')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+
+var app = express()
+const port = 3000
+
+// view engine setup
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.send('error in backend')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = app
---
+++ migrations/20181013144241_init.js
@@ -0,0 +1,12 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('Passage', function (t) {
+    t.increments('ID').unsigned().primary(),
+    t.string('Language'),
+    t.string('Tpoic'),
+    t.decimal('Payment')
+  })
+}
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('Passage')
+}
