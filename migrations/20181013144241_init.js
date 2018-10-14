a1ce060c6605f4435eb4b05e8759eabe3c48dc7a
zzq229 <zzqwaterloo@gmail.com>
2018-10-14 00:58:57 -0400

    fix error

--- migrations/20181013144241_init.js
+++ migrations/20181013144241_init.js
@@ -22,5 +22,5 @@ exports.up = function (knex, Promise) {
 }
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTable('Paragraph').drop('Passage')
+  return knex.schema.dropTable('Paragraph').dropTable('Passage')
 }
